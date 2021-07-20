b7686f1a60778ffe1de99361eb237ad763150a13
Halyna Urbanska <webenot@mail.ua>
2021-07-20 10:22:45 +0300

    feat: Add new meetup form frontend

--- application/src/pages/NewMeetup.js
+++ application/src/pages/NewMeetup.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import { NewMeetupForm } from '../components/meetups/NewMeetupForm';
+
 export const NewMeetupPage = () => (
-  <div>
-    New Meetup Page
-  </div>
+  <section>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm />
+  </section>
 );
